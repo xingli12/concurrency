3ba50240b37523096d9609e90708023282c47e9d
xingli12 <xingli12@iflytek.com>
2018-09-05 10:51:41 +0800

    Initial commit

---
+++ src/main/java/com/lx/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,27 @@
+package com.lx.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * description 课程里用来标记【线程安全】的类或者写法
+ * projectName  concurrency
+ * package com.lx.concurrency.annoations
+ *
+ * @author xingli12
+ * @version v1
+ * @date Created in 2018-09-05 10:34
+ * modified By
+ * updateDate
+ */
+//注解作用的目标
+@Target(ElementType.TYPE)
+//注解作用的范围
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+
+}
